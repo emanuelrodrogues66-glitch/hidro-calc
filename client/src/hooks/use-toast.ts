d924b5cc7151a8cb09999dd1ee62827d35f2c7f2
emanuelrodrogues66-glitch <emanuelrodrogues66@gmail.com>
2026-06-05 14:00:15 -0300

    Update use-toast.ts

--- client/src/hooks/use-toast.ts
+++ client/src/hooks/use-toast.ts
@@ -1,9 +1,12 @@
 import * as React from "react"
 
-import type {
-  ToastActionElement,
-  ToastProps,
-} from "@/components/ui/toast"
+type ToastProps = {
+  open?: boolean
+  onOpenChange?: (open: boolean) => void
+  variant?: "default" | "destructive"
+}
+
+type ToastActionElement = React.ReactElement
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
